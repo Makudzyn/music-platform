d5267423635e26cbc9ffceb2531d72275cf84381
Makudzyn <alt4xh0@gmail.com>
2024-10-02 13:55:45 +0300

    DB Scheme update and field fix. Resolved #26

--- server/src/track/schemas/comment.schema.ts
+++ server/src/track/schemas/comment.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { Track } from "./track.schema";
 
 export type CommentDocument = HydratedDocument<Comment>;
 
@@ -11,7 +10,7 @@ export class Comment {
   @Prop()
   text: string;
   @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Track'})
-  track: Track;
+  track: mongoose.Types.ObjectId;
 }
 
 export const CommentSchema = SchemaFactory.createForClass(Comment);
